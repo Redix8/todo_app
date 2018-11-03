1e20dbef3f1adbb1a9e01c6699ce5aa2e8ef53e7
Redix8 <40425965+Redix8@users.noreply.github.com>
2018-11-03 18:49:31 +0900

    Updates

---
+++ precache-manifest.544285d385fd79bb27b025c29b42a575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9881e67fb72b4b419ec0",
+    "url": "/todo_app/static/js/runtime~main.9881e67f.js"
+  },
+  {
+    "revision": "5a449bcae14ae3998658",
+    "url": "/todo_app/static/js/main.5a449bca.chunk.js"
+  },
+  {
+    "revision": "769ef06669047ddbf640",
+    "url": "/todo_app/static/js/1.769ef066.chunk.js"
+  },
+  {
+    "revision": "5a449bcae14ae3998658",
+    "url": "/todo_app/static/css/main.ff24a04f.chunk.css"
+  },
+  {
+    "revision": "d7c341d303ee0afdf845908389270d50",
+    "url": "/todo_app/index.html"
+  }
+];
